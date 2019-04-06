3963d6ed0dce57437eca665d07b51ebb6ad4ad5b
Louie Williford <dustyfingers01257@gmail.com>
2019-04-06 16:46:03 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = '8090';
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.listen(port, function() {
+  console.log('Server is running...');
+});
