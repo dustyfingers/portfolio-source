d4a3020c8c2b0ef9344b70073020a5fcf37f874c
Lambda_School_Loaner_184 <lambda_school_loaner_184@Lambda-School-Loaner-184s-MacBook-Pro.local>
2019-10-04 12:09:19 -0600

    fixed history problem when leaving website and returning

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/styles';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
-import styles from '../styles/LandingStyles';
-import Footer from './Footer';
-
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/styles";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import styles from "../styles/LandingStyles";
+import Footer from "./Footer";
 
 const Landing = ({ classes }) => (
-    <section className={classes.landingSection}>
-        <Typography className={classes.header}>
-            Hi!
-                </Typography>
-        <Typography className={classes.header}>
-            I'm Louie.
-                </Typography>
-        <Typography paragraph className={classes.paragraph}>
-            I'm a full stack web developer with a passion for learning about cutting edge technology.
-            When I'm not building nimble, scalable web apps with React & Redux, Node.js, Express, MongoDB and AWS, you can
-            find me at the park with my dogs or eating a home-cooked meal with my family!
-                </Typography>
-        <Typography paragraph className={`${classes.paragraph} ${classes.paragraph2}`}>
-            I currently live and work in
-            Colorado Springs, CO, but I am open to opportunities in any major city in the US.
-                </Typography>
-        <div className={classes.buttons}>
-            <Link to='/portfolio' className={classes.buttonLink}>
-                <Button variant="outlined" size='large' className={classes.button}>
-                    My Portfolio
-                </Button>
-            </Link>
-            <Link to='/resume' className={classes.buttonLink}>
-                <Button variant="outlined" size='large' className={classes.button}>
-                    My Resume
-                </Button>
-            </Link>
-        </div>
-        <Footer />
-    </section >
+  <section className={classes.landingSection}>
+    <Typography className={classes.header}>Hi!</Typography>
+    <Typography className={classes.header}>I'm Louie.</Typography>
+    <Typography paragraph className={classes.paragraph}>
+      I'm a full stack web developer with a passion for learning about cutting
+      edge technology. In particular, I know and use frequently: JS/ES6+,
+      Python, LESS/SASS, React, Redux, Node.js, Express, Django, MongoDB, SQL,
+      Heroku and AWS Amplify/Route53. When I'm not at building the software
+      solutions of the future you can find me at the park with my dogs, at the
+      gym practicing jiu jitsu, or at home playing guitar!
+    </Typography>
+    <Typography
+      paragraph
+      className={`${classes.paragraph} ${classes.paragraph2}`}
+    >
+      I currently live and work in Colorado Springs, CO, but I am open to
+      opportunities in any major city in the US.
+    </Typography>
+    <div className={classes.buttons}>
+      <Link to="/portfolio" className={classes.buttonLink}>
+        <Button variant="outlined" size="large" className={classes.button}>
+          My Portfolio
+        </Button>
+      </Link>
+      <Link to="/resume" className={classes.buttonLink}>
+        <Button variant="outlined" size="large" className={classes.button}>
+          My Resume
+        </Button>
+      </Link>
+    </div>
+    <Footer />
+  </section>
 );
-
 
 export default withStyles(styles)(Landing);
--- src/components/RightDrawer.js
+++ src/components/RightDrawer.js
@@ -1,81 +1,93 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import clsx from 'clsx';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import '../styles/App.css';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import clsx from "clsx";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import "../styles/App.css";
 
+const RightDrawer = ({ classes, children, open, setOpen, history }) => {
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
 
-const RightDrawer = ({ classes, children, open, setOpen }) => {
+  function handleDrawerClose() {
+    setOpen(false);
+  }
 
-    function handleDrawerOpen() {
-        setOpen(true);
-    }
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
+        <Toolbar>
+          <Typography variant="h3" noWrap className={classes.title}>
+            <Link to="/" className={`${classes.navLink} ${classes.Logo}`}>
+              {" "}
+              Louie Williford{" "}
+            </Link>
+          </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            className={clsx(open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      {children}
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronRightIcon />
+          </IconButton>
+        </div>
+        <List>
+          <ListItem
+            onClick={() => history.push("/")}
+            className={classes.navLink}
+            button
+          >
+            Home
+          </ListItem>
+          <ListItem
+            onClick={() => history.push("/portfolio")}
+            className={classes.navLink}
+            button
+          >
+            My Portfolio
+          </ListItem>
+          <ListItem
+            onClick={() => history.push("/resume")}
+            className={classes.navLink}
+            button
+          >
+            My Resume
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
+  );
+};
 
-    function handleDrawerClose() {
-        setOpen(false);
-    }
-
-    return (
-        <>
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
-                <Toolbar>
-                    <Typography variant="h3" noWrap className={classes.title}>
-                        <Link to='/' className={`${classes.navLink} ${classes.Logo}`}> Louie Williford </Link>
-                    </Typography>
-                    <IconButton
-                        color="inherit"
-                        aria-label="Open drawer"
-                        edge="end"
-                        onClick={handleDrawerOpen}
-                        className={clsx(open && classes.hide)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-            {children}
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="right"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        <ChevronRightIcon />
-                    </IconButton>
-                </div>
-                <List>
-                    <Link to='/' className={classes.navLink}>
-                        <ListItem button>Home</ListItem>
-                    </Link>
-                    <Link to='/portfolio' className={classes.navLink}>
-                        <ListItem button>My Portfolio</ListItem>
-                    </Link>
-                    <Link to='/resume' className={classes.navLink}>
-                        <ListItem button>My Resume</ListItem>
-                    </Link>
-                </List>
-            </Drawer>
-        </>
-
-    )
-}
-
-export default RightDrawer
+export default withRouter(RightDrawer);
