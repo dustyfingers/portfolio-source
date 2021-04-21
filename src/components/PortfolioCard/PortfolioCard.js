31b9361c7635dcff1b659381e5609e7036c497cf
lou <dustyfingers01257@gmail.com>
2021-04-21 16:22:06 -0600

    fixed cards to fit branding of site

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -4,14 +4,14 @@ import "./PortfolioCard.scss";
 
 const PortfolioCard = ({ description, title, image, siteLink, repoLink, showSiteLink, showRepoLink }) => {
     return (
-        <div className="card h-100">
+        <div className="portfolio-card card h-100">
             <img src={image} className="card-img-top" alt={title} />
-            <div className="card-body d-flex flex-column justify-content-between">
-                <h5 className="card-title portfolio-card-text">{title}</h5>
-                <p className="card-text portfolio-card-text">{description}</p>
+            <div className="portfolio-card-body card-body d-flex flex-column justify-content-between">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
                 <div className="buttons">
-                    {showSiteLink ? (<a href={siteLink} className="btn btn-primary ml-0 my-1 mr-1">Explore the Project</a>) : ''}
-                    {showRepoLink ? (<a href={repoLink} className="btn btn-primary m-1">See the Code</a>) : ''}
+                    {showSiteLink ? (<a href={siteLink} className="btn btn-primary ml-0 my-1 mr-1 portfolio-btn">Explore the Project</a>) : ''}
+                    {showRepoLink ? (<a href={repoLink} className="btn btn-primary m-1 portfolio-btn">See the Code</a>) : ''}
                 </div>
             </div>
         </div>
