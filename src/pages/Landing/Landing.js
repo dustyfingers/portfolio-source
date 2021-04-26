23a69fa335727842ca53427c744fca774dc0f696
lou <dustyfingers01257@gmail.com>
2021-04-25 18:30:14 -0600

    implemented simple animation

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -6,14 +6,14 @@ import "./Landing.scss";
 const Landing = () => (
 <section id="LandingSection" className="d-flex align-items-center text-center">
     <div className="landing-cta-section mx-auto">
-        <h1 className="display-1">Hi!</h1>
-        <h4 className="display-1">I'm Louie.</h4>
-        <p>
+        <h1 className="display-1 hi-tagline">Hi!</h1>
+        <h4 className="display-1 name-tagline">I'm Louie.</h4>
+        <p className="personal-summary">
             I'm a full stack software developer with a passion for learning about cutting
             edge technology. When I'm not building user-oriented software solutions, you can find me at the
             park with my beautiful family, in my home studio producing instrumentals, or at the gym practicing jiu jitsu!
         </p>
-        <p>I am currently looking for work in or around Denver, CO.</p>
+        <p className="work-line">I am currently looking for work in or around Denver, CO.</p>
 
         {/* call to action links */}
         <div>
