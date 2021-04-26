c3a99e0598da7a67f282512d11f41e866bf0a4fa
lou <dustyfingers01257@gmail.com>
2021-04-26 11:37:27 -0600

    customized landing page particles

--- src/data/particlesConfig.js
+++ src/data/particlesConfig.js
@@ -5,14 +5,46 @@ export default {
         },
         size: {
             value: 2
+        },
+        color: "#0E5159",
+        shape: {
+            type: "circle",
+            stroke: {
+              width: 3,
+              color: "#0E5159"
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 200,
+            color: "#0E5159",
+            opacity: 0.6,
+            width: 1
         }
     },
+
     interactivity: {
         events: {
             onhover: {
                 enable: true,
                 mode: "repulse"
+            },
+            onclick: {
+                enable: true,
+                mode: "push"
             }
-        }
+        },
+        modes: {
+            repulse: {
+              distance: 200,
+              duration: 0.4
+            },
+            push: {
+              particles_nb: 3
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
     }
 };
