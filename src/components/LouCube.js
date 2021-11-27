c8f4582bf2144b98cee65eb26c7b882863311879
lou <dustyfingers01257@gmail.com>
2021-11-27 11:53:56 -0700

    animated loucube

--- src/components/LouCube.js
+++ src/components/LouCube.js
@@ -6,19 +6,36 @@ import { useFrame } from '@react-three/fiber';
 
 export const LouCube = props => {
 
+  const meshRef = useRef();
+
   const colorMap = useLoader(TextureLoader, 'images/headshot.jpg');
 
+  // set off to side
+  // on scroll, animate position
+
+  const handleWheel = evt => {
+    console.log('wheelin and dealin!');
+  }
+
+  // rotate cube every frame
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.z += 0.01;
+  })
+
   return (
-    <mesh>
+    <mesh 
+      ref={meshRef}
+      onWheel={evt => handleWheel()}>
       <boxGeometry args={[1, 1, 1]} />
       {/* <meshPhongMaterial specularMap={specularMap} /> */}
       {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
       {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
       <meshStandardMaterial map={colorMap} />
       <OrbitControls 
-        enableZoom={true} 
+        enableZoom={false} 
         enalbePan={true} 
-        enableRatate={true}
+        enableRotate={true}
         zoomSpeed={0.6}
         panSpeed={0.5}
         rotateSpeed={0.4} />
