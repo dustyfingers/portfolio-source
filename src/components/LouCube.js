a8f96d491b3b0d6e49d0525ca7e835578d4d6f8c
lou <dustyfingers01257@gmail.com>
2021-11-27 12:19:16 -0700

    added basic starfield, working on controls

--- src/components/LouCube.js
+++ src/components/LouCube.js
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react';
 import { useLoader } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
 import { TextureLoader } from 'three';
 import { useFrame } from '@react-three/fiber';
 
@@ -26,19 +25,13 @@ export const LouCube = props => {
   return (
     <mesh 
       ref={meshRef}
+      {...props}
       onWheel={evt => handleWheel()}>
       <boxGeometry args={[1, 1, 1]} />
       {/* <meshPhongMaterial specularMap={specularMap} /> */}
       {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
       {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
       <meshStandardMaterial map={colorMap} />
-      <OrbitControls 
-        enableZoom={false} 
-        enalbePan={true} 
-        enableRotate={true}
-        zoomSpeed={0.6}
-        panSpeed={0.5}
-        rotateSpeed={0.4} />
     </mesh>
   )
 }
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react';
 // need uuid later dont delete bozo
 // import { v4 as uuid } from 'uuid';
 import { Canvas } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+import { MapControls, Stars } from '@react-three/drei';
 
 // resume stuff
 // import ResumeProjectItem from '../../components/ResumeProjectItem';
@@ -10,7 +12,7 @@ import { Canvas } from '@react-three/fiber';
 // import Skill from '../../components/Skill';
 // import resumeData from '../../data/data';
 
-// three.js stuff
+// three.js components
 import { LouCube } from '../../components/LouCube';
 
 import './Resume.scss';
@@ -25,9 +27,23 @@ const Resume = () => {
       <Suspense fallback={null}>
         <div id="CanvasContainer">
           <Canvas>
+
+            <Stars
+              radius={100} // Radius of the inner sphere (default=100)
+              depth={50} // Depth of area where stars should fit (default=50)
+              count={5000} // Amount of stars (default=5000)
+              factor={2} // Size factor (default=4)
+              saturation={0} // Saturation 0-1 (default=0)
+              fade // Faded dots (default=false)
+            />
+
             <ambientLight intensity={1} />
-            <directionalLight color="red" position={[0, 0, 5]} />
-            <LouCube />
+
+            {/* <directionalLight position={[0, 0, 5]} /> */}
+
+            <LouCube position={[3.5,0,0]} />
+
+            <MapControls />
 
           </Canvas>
         </div>
