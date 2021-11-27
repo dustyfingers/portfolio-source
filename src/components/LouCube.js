7f6be3537a7afad58a8d3f4800ddceb9c6068209
lou <dustyfingers01257@gmail.com>
2021-11-27 11:43:48 -0700

    built basic lou cube

--- src/components/LouCube.js
+++ src/components/LouCube.js
@@ -1,44 +1,27 @@
 import React, { useRef, useState } from 'react';
 import { useLoader } from '@react-three/fiber';
-// import { OrbitControls } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import { TextureLoader } from 'three';
 import { useFrame } from '@react-three/fiber';
 
 export const LouCube = props => {
 
-  const mesh = useRef();
-
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
-
   const colorMap = useLoader(TextureLoader, 'images/headshot.jpg');
 
   return (
-    <>
-      <mesh 
-        {...props}
-        ref={mesh}
-        scale={2}
-        onClick={(event) => setActive(!active)}
-        onPointerOver={(event) => setHover(true)}
-        onPointerOut={(event) => setHover(false)}
-        >
-          <boxGeometry args={[10, 10, 10]} />
-          {/* <meshPhongMaterial specularMap={specularMap} /> */}
-          {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
-          {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
-          <meshStandardMaterial map={colorMap} />
-          {/* <OrbitControls 
-            enableZoom={true} 
-            enalbePan={true} 
-            enableRatate={true}
-            zoomSpeed={0.6}
-            panSpeed={0.5}
-            rotateSpeed={0.4} /> */}
-      </mesh>
-    </>
+    <mesh>
+      <boxGeometry args={[1, 1, 1]} />
+      {/* <meshPhongMaterial specularMap={specularMap} /> */}
+      {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
+      {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
+      <meshStandardMaterial map={colorMap} />
+      <OrbitControls 
+        enableZoom={true} 
+        enalbePan={true} 
+        enableRatate={true}
+        zoomSpeed={0.6}
+        panSpeed={0.5}
+        rotateSpeed={0.4} />
+    </mesh>
   )
 }
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -29,14 +29,6 @@ const Resume = () => {
             <directionalLight color="red" position={[0, 0, 5]} />
             <LouCube />
 
-            {/* straight from the docs... */}
-            <ambientLight intensity={0.1} />
-            <directionalLight color="red" position={[0, 0, 5]} />
-            <mesh>
-              <boxGeometry />
-              <meshStandardMaterial />
-            </mesh>
-
           </Canvas>
         </div>
 
