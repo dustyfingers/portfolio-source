e43eb1b7272b2a1ea79725d52565efddbe9a0b73
lou <dustyfingers01257@gmail.com>
2022-01-23 04:00:01 -0700

    working on adding scroll animation to loucube

--- src/components/LouCube.js
+++ src/components/LouCube.js
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
-import { useLoader } from '@react-three/fiber';
+import React, { useEffect, useRef } from 'react';
+import { useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader } from 'three';
-import { useFrame } from '@react-three/fiber';
+
+import wScroll from '../classes/wScroll';
 
 export const LouCube = props => {
 
@@ -12,25 +13,59 @@ export const LouCube = props => {
   // set off to side
   // on scroll, animate position
 
-  const handleWheel = evt => {
-    console.log('wheelin and dealin!');
+  const handleScroll = (topOffset) => {
+
+    const scroller = new wScroll();
+    
+    // this is kind of broken but usable for now
+    const percentage = scroller.get_scroll_percentage();
+
+    console.log({percentage})
+    console.log({topOffset})
+
+    // no top offset means we are at the top of the document
+    if (!topOffset) {
+      meshRef.current.position.x = 8;
+      meshRef.current.position.y = 1;
+      meshRef.current.position.z = -2;
+      
+    }
+
+    // else if (percentage === 100) {
+    //   meshRef.current.position.x = 8;
+    //   meshRef.current.position.y = 1;
+    //   meshRef.current.position.z = -2;
+    // }
+
+    else {
+      meshRef.current.position.y = meshRef.current.position.y + (topOffset / 100) * -1;
+    }
+
   }
+
+  const scrollEvent = () => handleScroll(window.scrollY);
 
   // rotate cube every frame
   useFrame((state, delta) => {
+
+    // handle rotation through frames
     meshRef.current.rotation.x += 0.01;
     meshRef.current.rotation.z += 0.01;
+
   })
+
+  useEffect(() => document.addEventListener('scroll', scrollEvent), [])
+
+  useEffect(() => () => document.removeEventListener('scroll', scrollEvent), [])
 
   return (
     <mesh 
       ref={meshRef}
-      {...props}
-      onWheel={evt => handleWheel()}>
-      <boxGeometry args={[1, 1, 1]} />
-      {/* <meshPhongMaterial specularMap={specularMap} /> */}
-      {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
-      {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
+      {...props} >
+        <boxGeometry args={[1, 1, 1]} />
+        {/* <meshPhongMaterial specularMap={specularMap} /> */}
+        {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
+        {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
       <meshStandardMaterial map={colorMap} />
     </mesh>
   )
