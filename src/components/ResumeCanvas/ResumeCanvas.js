8571aec3ecd32ba76f0fdad86e143ae3bcfeb6d6
lou <dustyfingers01257@gmail.com>
2022-01-23 01:18:16 -0700

    remove loucube for staging push

--- src/components/ResumeCanvas/ResumeCanvas.js
+++ src/components/ResumeCanvas/ResumeCanvas.js
@@ -1,7 +1,6 @@
-import React, { useRef } from 'react';
-
+import React, { useRef, createRef, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Stars } from '@react-three/drei';
+import { Stars, ScrollControls, Scroll } from '@react-three/drei';
 
 // custom three.js components
 import { LouCube } from '../../components/LouCube';
@@ -10,11 +9,12 @@ import './ResumeCanvas.scss';
 
 const ResumeCanvas = () => {
   // const [zoom, setZoom] = useState(75)
+  // const [top, createTop] = useState(createRef())
 
 
   // const scrollArea = useRef()
-  // const onScroll = (e) => (state.top.current = e.target.scrollTop)
-  // useEffect(() => void onScroll({ target: scrollArea.current }), [])
+  // const onScroll = (e) => (top.current = e.target.scrollTop)
+  // useEffect(() => onScroll({ target: scrollArea.current }), [])
 
   return (
 
@@ -22,28 +22,32 @@ const ResumeCanvas = () => {
 
       <Canvas id="ResumeCanvas" dpr={window.devicePixelRatio}>
 
-        <Stars
-          radius={100} // Radius of the inner sphere (default=100)
-          depth={50} // Depth of area where stars should fit (default=50)
-          count={5000} // Amount of stars (default=5000)
-          factor={2} // Size factor (default=4)
-          saturation={0} // Saturation 0-1 (default=0)
-          fade // Faded dots (default=false)
-        />
-        
-        <color attach="background" args={['#020e17']} />
+        {/* <ScrollControls> */}
 
-        <ambientLight intensity={1} />
+          <Stars
+            radius={100} // Radius of the inner sphere (default=100)
+            depth={50} // Depth of area where stars should fit (default=50)
+            count={5000} // Amount of stars (default=5000)
+            factor={2} // Size factor (default=4)
+            saturation={0} // Saturation 0-1 (default=0)
+            fade // Faded dots (default=false)
+          />
+          
+          <color attach="background" args={['#020e17']} />
 
-        {/* <directionalLight position={[0, 0, 5]} /> */}
+          <ambientLight intensity={1} />
 
-        <LouCube position={[8,1,-2]} />
+          {/* <directionalLight position={[0, 0, 5]} /> */}
+
+          {/* <LouCube position={[8,1,-2]} /> */}
+
+          {/* <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+            <div style={{ height: `100vh` }} />
+          </div> */}
+
+        {/* </ScrollControls> */}
 
       </Canvas>
-
-      {/* <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
-        <div style={{ height: `${state.pages * 100}vh` }} />
-      </div> */}
 
     </div>
 
