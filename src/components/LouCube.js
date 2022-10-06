48fa14d2a4d9717fd25d79ba1e64509ff3140443
lou <dustyfingers01257@gmail.com>
2022-10-06 02:43:07 -0600

    fix problems from updating react, work on fixing particles

--- src/components/LouCube.js
+++ src/components/LouCube.js
@@ -2,76 +2,18 @@ import React, { useEffect, useRef } from 'react';
 import { useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader } from 'three';
 
-import wScroll from '../classes/wScroll';
+// import wScroll from '../classes/wScroll';
 
 export const LouCube = props => {
+    return (
+        <></>
+        // <mesh ref={meshRef} {...props}>
+        //     <boxGeometry args={[1, 1, 1]} />
+        //     {/* <meshPhongMaterial specularMap={specularMap} /> */}
+        //     {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
+        //     {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
 
-  const meshRef = useRef();
-
-  const colorMap = useLoader(TextureLoader, 'images/headshot.jpg');
-
-  // set off to side
-  // on scroll, animate position
-
-//   const handleScroll = (topOffset) => {
-
-//     const scroller = new wScroll();
-    
-//     // this is kind of broken but usable for now
-//     const percentage = scroller.get_scroll_percentage();
-
-//     // console.log({percentage})
-//     // console.log({topOffset})
-
-//     // no top offset means we are at the top of the document
-//     if (!topOffset) {
-//       meshRef.current.position.x = 8;
-//       meshRef.current.position.y = 1;
-//       meshRef.current.position.z = -2;
-      
-//     }
-
-//     // percentage at 100 means we are at the bottom of the document
-//     // else if (percentage === 100) {
-//     //   meshRef.current.position.x = 8;
-//     //   meshRef.current.position.y = 1;
-//     //   meshRef.current.position.z = -2;
-//     // }
-
-//     else {
-//       meshRef.current.position.y = meshRef.current.position.y + (topOffset / 100) * -1;
-//     }
-
-//   }
-
-//   const scrollEvent = () => handleScroll(window.scrollY);
-
-  // rotate cube every frame
-  useFrame((state, delta) => {
-
-    // handle rotation through frames
-    meshRef.current.rotation.x += 0.01;
-    meshRef.current.rotation.z += 0.01;
-
-  })
-
-//   useEffect(() => document.addEventListener('scroll', scrollEvent), [])
-
-//   useEffect(() => () => document.removeEventListener('scroll', scrollEvent), [])
-
-  return (
-
-    <mesh 
-        ref={meshRef}
-        {...props} >
-
-            <boxGeometry args={[1, 1, 1]} />
-            {/* <meshPhongMaterial specularMap={specularMap} /> */}
-            {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
-            {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
-
-        <meshStandardMaterial map={colorMap} />
-
-    </mesh>
-  )
-}
+        //     <meshStandardMaterial map={colorMap} />
+        // </mesh>
+    );
+};
--- src/components/ResumeCanvas/ResumeCanvas.js
+++ src/components/ResumeCanvas/ResumeCanvas.js
@@ -3,55 +3,27 @@ import { Canvas } from '@react-three/fiber';
 import { Stars, PresentationControls } from '@react-three/drei';
 
 // custom three.js components
-import { LouCube } from '../../components/LouCube';
+// import { LouCube } from '../../components/LouCube';
 
 import './ResumeCanvas.scss';
 
 const ResumeCanvas = () => {
+    return (
+        <div id="ResumeCanvasContainer" /* ref={ref} */>
+            {/* <Canvas id="ResumeCanvas" dpr={window.devicePixelRatio}> */}
+            {/* background */}
+            {/* <Stars
+                    radius={100} // Radius of the inner sphere (default=100)
+                    depth={50} // Depth of area where stars should fit (default=50)
+                    count={5000} // Amount of stars (default=5000)
+                    factor={2} // Size factor (default=4)
+                    saturation={0} // Saturation 0-1 (default=0)
+                    fade // Faded dots (default=false)
+                /> */}
 
-  return (
-
-    <div id="ResumeCanvasContainer" /* ref={ref} */ >
-
-      <Canvas id="ResumeCanvas" dpr={window.devicePixelRatio}>
-
-        {/* background */}
-        <Stars
-          radius={100} // Radius of the inner sphere (default=100)
-          depth={50} // Depth of area where stars should fit (default=50)
-          count={5000} // Amount of stars (default=5000)
-          factor={2} // Size factor (default=4)
-          saturation={0} // Saturation 0-1 (default=0)
-          fade // Faded dots (default=false)
-        />
-
-        <color attach="background" args={['#020e17']} />
-
-        {/* lighting */}
-        <ambientLight intensity={1} />
-
-        {/* <directionalLight position={[0, 0, 5]} /> */}
-
-        <PresentationControls
-          global={false} // Spin globally or by dragging the model
-          snap={false} // Snap-back to center (can also be a spring config)
-          speed={1} // Speed factor
-          zoom={1} // Zoom factor when half the polar-max is reached
-          rotation={[0, 0, 0]} // Default rotation
-          polar={[0, Math.PI / 2]} // Vertical limits
-          azimuth={[-Infinity, Infinity]} // Horizontal limits
-          config = { { mass: 1, tension: 170, friction: 26 } } >
-            
-            {/* scene objects */}
-            {/* <LouCube position={[8,1,-2]} /> */}
-
-        </PresentationControls>
-
-      </Canvas>
-
-    </div>
-
-  );
+            {/* </Canvas> */}
+        </div>
+    );
 };
 
 export default ResumeCanvas;
