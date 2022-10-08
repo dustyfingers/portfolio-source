6e27eb23e3794f13d3b72d895af568d96581c7cd
lou <dustyfingers01257@gmail.com>
2022-10-08 01:41:59 -0600

    work on getting canvas working again

--- src/components/LouCube.js
+++ src/components/LouCube.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useRef } from 'react';
 import { useLoader, useFrame } from '@react-three/fiber';
-import { TextureLoader } from 'three';
+import * as THREE from 'three';
 
 // import wScroll from '../classes/wScroll';
 
-export const LouCube = props => {
+const LouCube = props => {
     return (
-        <></>
-        // <mesh ref={meshRef} {...props}>
-        //     <boxGeometry args={[1, 1, 1]} />
-        //     {/* <meshPhongMaterial specularMap={specularMap} /> */}
-        //     {/* normal map gets passedf in as prop to meshStandardMaterial like this */}
-        //     {/* <meshStandardMaterial map={colorMap} normalMap={normalMap} /> */}
-
-        //     <meshStandardMaterial map={colorMap} />
-        // </mesh>
+        <mesh castShadow reveiveShadow position={[0, 0, 0]}>
+            <boxGeometry attach="geometry" args={[2, 2, 2]} />
+            <meshStandardMaterial
+                attach="material"
+                emissive={new THREE.Color(0xff0000)}
+                emissiveIntensity={1.5}
+            />
+        </mesh>
     );
 };
+
+export default LouCube;
--- src/components/ResumeCanvas/ResumeCanvas.js
+++ src/components/ResumeCanvas/ResumeCanvas.js
@@ -1,28 +1,100 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
-import { Stars, PresentationControls } from '@react-three/drei';
+import { Stars, PerspectiveCamera, PresentationControls } from '@react-three/drei';
+import { EffectComposer, Noise } from '@react-three/postprocessing';
+import { motion, useScroll, useInView } from 'framer-motion';
 
 // custom three.js components
-// import { LouCube } from '../../components/LouCube';
+import LouCube from '../LouCube';
+import ResumeContent from '../ResumeContent/ResumeContent';
+
+// using motion with a custom component
+// const AnimatedComponent = motion.custom(CustomComponent);
 
 import './ResumeCanvas.scss';
 
 const ResumeCanvas = () => {
-    return (
-        <div id="ResumeCanvasContainer" /* ref={ref} */>
-            {/* <Canvas id="ResumeCanvas" dpr={window.devicePixelRatio}> */}
-            {/* background */}
-            {/* <Stars
-                    radius={100} // Radius of the inner sphere (default=100)
-                    depth={50} // Depth of area where stars should fit (default=50)
-                    count={5000} // Amount of stars (default=5000)
-                    factor={2} // Size factor (default=4)
-                    saturation={0} // Saturation 0-1 (default=0)
-                    fade // Faded dots (default=false)
-                /> */}
+    // const { scrollY } = useScroll();
 
-            {/* </Canvas> */}
-        </div>
+    // useEffect(() => console.log(scrollY), [scrollY]);
+
+    return (
+        <>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <p>heloasdaosdoadsdasd</p>
+            <Canvas id="ResumeCanvas" dpr={window.devicePixelRatio}>
+                <gridHelper />
+                <PerspectiveCamera
+                    makeDefault
+                    fov={45}
+                    aspect={window.innerWidth / window.innerHeight}
+                    near={1}
+                    far={1000}
+                    position={new THREE.Vector3(2, 0, 0)}
+                    // rotation={new THREE.Euler(-0.7, -0.2, -0.2)}
+                />
+                <LouCube position={[0, 0, 0]} />
+                <LouCube position={[0, 0, -2]} />
+                {/* <color attach="background" args={['red']} />
+                <PresentationControls>
+                    <Stars
+                        radius={100} // Radius of the inner sphere (default=100)
+                        depth={50} // Depth of area where stars should fit (default=50)
+                        count={5000} // Amount of stars (default=5000)
+                        factor={2} // Size factor (default=4)
+                        saturation={0} // Saturation 0-1 (default=0)
+                        fade // Faded dots (default=false)
+                    />
+
+                    <ambientLight intensity={0.1} />
+                    <directionalLight color="red" position={[0, 0, 5]} />
+                </PresentationControls>
+                <EffectComposer>
+                    <Noise opacity={0.9} />
+                </EffectComposer> */}
+            </Canvas>
+            <div>hellopp</div>
+            <div>hellopp</div>
+            <div>hellopp</div>
+            <div>hellopp</div> <div>hellopp</div> <div>hellopp</div> <div>hellopp</div>{' '}
+            <div>hellopp</div> <div>hellopp</div> <div>hellopp</div> <div>hellopp</div>{' '}
+            <div>hellopp</div> <div>hellopp</div>
+        </>
     );
 };
 
