389641f7623c7ef6e2417650f3c7562b459799c5
lou <dustyfingers01257@gmail.com>
2022-10-08 01:41:59 -0600

    work on getting canvas working again

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,51 +5,49 @@ import './Menu.scss';
 
 const Menu = () => {
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="Menu">
-                <div className="container-fluid">
-                    <Link to="/" id="logo">
-                        <img height="40px" src="/images/logo.svg" alt="logo" />
-                    </Link>
+        <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="Menu">
+            <div className="container-fluid">
+                <Link to="/" id="logo">
+                    <img height="40px" src="/images/logo.svg" alt="logo" />
+                </Link>
 
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarDropdown"
-                        aria-controls="navbarDropdown"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <img
-                            className="navbar-toggler-icon"
-                            src="/images/list.svg"
-                            alt="hamburger menu button"
-                        />
-                    </button>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarDropdown"
+                    aria-controls="navbarDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <img
+                        className="navbar-toggler-icon"
+                        src="/images/list.svg"
+                        alt="hamburger menu button"
+                    />
+                </button>
 
-                    <div className="collapse navbar-collapse" id="navbarDropdown">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item d-flex flex-row-reverse">
-                                <Link to="/" className="nav-link">
-                                    Home
-                                </Link>
-                            </li>
-                            <li className="nav-item d-flex flex-row-reverse">
-                                <Link to="/resume" className="nav-link">
-                                    Resume
-                                </Link>
-                            </li>
-                            <li className="nav-item d-flex flex-row-reverse">
-                                <Link to="/portfolio" className="nav-link">
-                                    Portfolio
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                <div className="collapse navbar-collapse" id="navbarDropdown">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item d-flex flex-row-reverse">
+                            <Link to="/" className="nav-link">
+                                // Home
+                            </Link>
+                        </li>
+                        <li className="nav-item d-flex flex-row-reverse">
+                            <Link to="/resume" className="nav-link">
+                                // Resume
+                            </Link>
+                        </li>
+                        <li className="nav-item d-flex flex-row-reverse">
+                            <Link to="/portfolio" className="nav-link">
+                                // Portfolio
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     );
 };
 
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,28 +1,15 @@
 import React, { Suspense } from 'react';
 
-// resume components
-import ResumeContent from '../../components/ResumeContent/ResumeContent';
 import ResumeCanvas from '../../components/ResumeCanvas/ResumeCanvas';
-
 import './Resume.scss';
 
 const Resume = () => {
-
-  return (
-    <>
-      {/* TODO: fallback needs to be a LoadingWheel component */}
-      <Suspense fallback={null}>
-
-        <div id="ResumeContainer">
-
-          <ResumeCanvas />
-
-          <ResumeContent />
-          
-        </div>
-
-      </Suspense>
-    </>
-)};
+    return (
+        // TODO: fallback needs to be a LoadingWheel component
+        <Suspense fallback={null}>
+            <ResumeCanvas />
+        </Suspense>
+    );
+};
 
 export default Resume;
