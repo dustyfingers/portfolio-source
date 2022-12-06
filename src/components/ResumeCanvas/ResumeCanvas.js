f014fef1e40c443765e1898bae385f49d64c6326
lou <dustyfingers01257@gmail.com>
2022-12-06 00:55:25 -0700

    wrote a simple clump sim poc for the 3D scene

--- src/components/ResumeCanvas/ResumeCanvas.js
+++ src/components/ResumeCanvas/ResumeCanvas.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
-import { Stars, PerspectiveCamera, PresentationControls, Html } from '@react-three/drei';
-import { EffectComposer, Noise } from '@react-three/postprocessing';
-import { motion, useScroll, useInView } from 'framer-motion';
+import { PerspectiveCamera, Html } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
 
 // custom three.js components
 import LouCube from '../LouCube';
 import ResumeContent from '../ResumeContent/ResumeContent';
+import EffectsRig from '../EffectsRig/EffectsRig';
 
 import './ResumeCanvas.scss';
+import Clump from '../Clump/Clump';
+import Pointer from '../Pointer/Pointer';
 
 const ResumeCanvas = () => {
     // const { scrollY } = useScroll();
@@ -27,29 +29,18 @@ const ResumeCanvas = () => {
                     near={1}
                     far={1000}
                     position={new THREE.Vector3(0, 2, 14)}
-                    // rotation={new THREE.Euler(-0.7, -0.2, -0.2)}
                 />
-                <LouCube position={[0, 0, 0]} />
-                <Html center>
+                {/* <Html center>
                     <ResumeContent />
-                </Html>
-                {/* <color attach="background" args={['red']} />
-                <PresentationControls>
-                    <Stars
-                        radius={100} // Radius of the inner sphere (default=100)
-                        depth={50} // Depth of area where stars should fit (default=50)
-                        count={5000} // Amount of stars (default=5000)
-                        factor={2} // Size factor (default=4)
-                        saturation={0} // Saturation 0-1 (default=0)
-                        fade // Faded dots (default=false)
-                    />
-
-                    <ambientLight intensity={0.1} />
-                    <directionalLight color="red" position={[0, 0, 5]} />
-                </PresentationControls>
-                <EffectComposer>
-                    <Noise opacity={0.9} />
-                </EffectComposer> */}
+                </Html> */}
+                <color attach="background" args={['blue']} />
+                <ambientLight intensity={0.1} />
+                <directionalLight color="red" position={[0, 0, 5]} />
+                <Physics gravity={[0, 2, 0]} iterations={40}>
+                    <Pointer />
+                    <Clump />
+                </Physics>
+                <EffectsRig />
             </Canvas>
         </>
     );
