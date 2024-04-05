b323e872507d9757b5773f94fe89016e8451e492
lou <dustyfingers01257@gmail.com>
2024-04-05 16:09:32 -0600

    work on mobile nav and resume page a bit

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,13 +1,15 @@
 import React, { Suspense } from 'react';
 
 import ResumeCanvas from '../../components/ResumeCanvas/ResumeCanvas';
+import ResumeContent from '../../components/ResumeContent/ResumeContent';
 import './Resume.scss';
 
 const Resume = () => {
     return (
         // TODO: fallback needs to be a LoadingWheel component
         <Suspense fallback={null}>
-            <ResumeCanvas />
+            {/* <ResumeCanvas /> */}
+            <ResumeContent />
         </Suspense>
     );
 };
