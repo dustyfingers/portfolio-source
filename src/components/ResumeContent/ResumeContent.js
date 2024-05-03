9542381583e7fe07ca0cc8acbb63d596ff736544
lou <dustyfingers01257@gmail.com>
2024-05-03 11:27:56 -0600

    fix resume content headers

--- src/components/ResumeContent/ResumeContent.js
+++ src/components/ResumeContent/ResumeContent.js
@@ -16,15 +16,10 @@ const ResumeContent = () => {
     return (
         <div id="Resume" className="container">
             {/* name/title section */}
-            <motion.div
-                className="text-center mb-5"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transform={{ type: 'spring', duration: 2000, ease: 'easeOut' }}
-            >
+            <div className="text-center mb-5">
                 <h1 className="display-1">{name}</h1>
                 <h3>{title}</h3>
-            </motion.div>
+            </div>
             {/* experience section */}
             <div className="mb-5">
                 <h3 className="text-center">Experience</h3>
@@ -33,32 +28,27 @@ const ResumeContent = () => {
                 ))}
             </div>
             {/* education section */}
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transform={{ duration: 3, ease: 'easeOut' }}
-                className="d-flex flex-column align-items-center justify-content-center mb-5"
-            >
+            <div className="d-flex flex-column align-items-center justify-content-center mb-5">
                 <h3 className="text-center">Education</h3>
                 <div>
                     <p className="fs-4 text-center">
                         {education.school} - {education.program}
                     </p>
-                    <p className="text-center">
+                    <p className="text-center mb-0">
                         {education.from} - {education.to}
                     </p>
                 </div>
-            </motion.div>
+            </div>
             {/* projects section */}
             <div className="mb-5">
-                <p className="display-5 text-center">Projects</p>
+                <h3 className="text-center">Projects</h3>
                 {projects.map((project, idx) => (
                     <ResumeProjectItem {...project} key={`project-item-${uuid()}`} />
                 ))}
             </div>
             {/* skills section */}
             <div className="d-flex flex-column align-items-center justify-content-center mb-5">
-                <p className="display-5 text-center">Top Skills</p>
+                <h3 className="text-center">Skills</h3>
                 <div className="d-flex flex-wrap justify-content-evenly align-items-center skills w-75">
                     {skills.map((skill, idx) => (
                         <Skill skill={skill} key={`skill-item-${uuid()}`} />
